9f2f562f007a2c82e5b7fc08dad648dd7b079fe7
SonaliKhodave <khodavesonali@gmail.com>
2023-02-06 12:20:59 +0000

    Initial commit

---
+++ githubdemo1/src/main/java/com/test/pages/loginpage.java
@@ -0,0 +1,8 @@
+package com.test.pages;
+
+public class loginpage {
+
+    public void LoginPage(){
+        System.out.println("this is just a printing ");
+    }
+}
---
+++ githubdemo1/src/test/java/com/testcases/logintest.java
@@ -0,0 +1,7 @@
+package com.testcases;
+
+public class logintest {
+    public void Logintestcase(){
+        System.out.println("test caslss this is ");
+    }
+}
